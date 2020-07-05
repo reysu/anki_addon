e01ee2657dc359dcef8bbd3e10aa8d2de2977f8a
reysu <reysu@reysu.local>
2020-07-05 00:07:21 -0700

    sentenceConvert javascript template

---
+++ myaddon/sentenceConverter.js.py
@@ -0,0 +1,9 @@
+var field = document.querySelectorAll('.test');
+field[0].style.backgroundColor = "red";
+sentence = field.innerHTML;
+sentence = furiganify(sentence);
+field[0].innerHTML = sentence
+
+function furiganify(sentence) {
+       return "test";
+} 
