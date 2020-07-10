cb47257a112769ccea359d59f323364e0ced8c83
reysu <reysu@reysu.local>
2020-07-10 13:25:17 -0700

    curly brakcets

--- myaddon/sentenceConverter.js
+++ myaddon/sentenceConverter.js
@@ -17,18 +17,44 @@ function furiganify(sentence) {
        return sentence;
 }
 function furgianifyBrackets(sentence){
+      //the way that it is right now is like you have to add the tags
+      //nofurigana in order for the furigana to be invisible on the front
+
        var regex;
        var tempWord = "";
        regex = sentence.match(/[^ ]+?\[[^\]]+\]*/g);
        if(regex){
       	 for(var i=0; i<regex.length; i++){
            	furigana = regex[i].match(/\[([^\]]+)\]/)[1];
-		baseWord = regex[i].replace(/\[([^\]]+)\]/, "")
-		html = "<ruby>" + baseWord + "<rt>" + furigana + "</rt> </ruby>"
-		sentence = sentence.replace(/[^ ]+?\[[^\]]+\]*/g, html)
+        		baseWord = regex[i].replace(/\[([^\]]+)\]/, "")
+        		furiganaWBrackets = baseWord + regex[i].match(/\[([^\]]+)\]/)[0];
+        		html = "<ruby>" + baseWord + "<rt>" + furigana + "</rt> </ruby>"
+        		//the following line removes the brackets and the word
+        		//sentence = sentence.split(" "+ furiganaWBrackets).join("");
+        		//the following line replaces the brackets with the furigana
+        		sentence = sentence.split(" "+ furiganaWBrackets).join(html);
+            //the following was the old one taht used regex
+        		//sentence = sentence.replace(/[^ ]+?\[[^\]]+\]*/g, html)
 		}
        }
        return sentence;
+}
+
+//there's like some bug with when you have something on a new Line
+//it makes the thing super high.. 
+function curlyBrackets(sentence){
+  var regex;
+  var tempWord = "";
+  regex = sentence.match(/[^ ]+?\{[^\}]+\}*/g);
+  if(regex){
+    for(var i=0; i<regex.length; i++){
+      furigana = regex[i].match(/\{([^\}]+)\}/)[1];
+      baseWord = regex[i].replace(/\{([^\}]+)\}/, "")
+      html = "<ruby>" + baseWord + "<rt>" + furigana + "</rt> </ruby>";
+      sentence = sentence.split(regex[i]).join(html);
+    }
+  }
+  return sentence;
 }
 /*
 Basically the goal here is to take a sentence like
@@ -38,5 +64,10 @@ and convert it to
 which will convert it into furgiana
 
 todo: use regex
+todo: make it so it works with {} brackets instead of the other one
+todo: make it so you can put :furigana in anki brackets for it to actually work
+todo: make it add to all the script code on all the cards
+todo: make a gui using qt in order to select the decks that you want this added to
+
 
 */
