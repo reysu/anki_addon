caa9b62ee33804de427d42745d450d471dbc2ef6
reysu <reysu@reysu.local>
2020-07-18 12:24:39 -0700

    hook to inject javascript

--- myaddon/__init__.py
+++ myaddon/__init__.py
@@ -2,15 +2,43 @@ from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
 from . import model
-
+from anki.hooks import addHook
 
 def testFunction():
     cardCount = mw.col.cardCount()
     showInfo("Card count: %d" % cardCount)
-from . import model
+
 
 action = QAction("test",mw)
 #action.triggered.connect(testFunction)
 #mw.form.menuTools.addAction(action)
 
 
+def prepare(html, card, context):
+    return html + """
+var field = document.querySelectorAll('.test');
+sentence = field[0].innerHTML;
+sentence = furgianifyBrackets(sentence);
+field[0].innerHTML = sentence;
+<script>
+function curlyBrackets(sentence){
+  var regex;
+  var tempWord = "";
+  regex = sentence.match(/[^ ]+?\{[^\}]+\}*/g);
+  if(regex){
+    for(var i=0; i<regex.length; i++){
+      full = regex[i].match(/[^>]+?\{[^\}]+\}/).toString();
+      furigana = full.match(/\{([^\}]+)\}/)[1];
+      baseWord = full.replace(/\{([^\}]+)\}/, "")
+
+      html = "<ruby>" + baseWord + "<rt>" + furigana + "</rt> </ruby>";
+    // html = "<ruby> hey<rt>你好</rt></ruby>"
+     sentence = sentence.split(full).join(html);
+   // sentence = typeof full
+
+    }
+  }
+  return sentence;
+}
+</script>"""
+addHook('prepareQA', prepare)
